391ab6b1662496dce73e5c1650d145ca49068798
Анастасия <kamanina963@gmail.com>
2021-12-28 19:36:05 +0300

    refactor: Добавь инфо о мин/макс стоимости в State
    Для того, чтобы информация о цене была доступна на верхнем уровне
    фильтра без "опрокидывания" стейта фильтра через пропсы

--- src/store/api-actions.ts
+++ src/store/api-actions.ts
@@ -23,11 +23,33 @@ const fetchSortOrderAction = (type: string): ThunkActionResult =>
     } catch {
       // TODO: в случае недоступности сервера отображается информационное сообщение
       // eslint-disable-next-line no-console
-      console.log('не удалось загрузить данные');
+      console.log('не удалось загрузить отсортированные данные');
+    }
+  };
+
+const fetchFilterAction = (type: string): ThunkActionResult =>
+  async (dispatch, _getState, api): Promise<void> => {
+    try {
+      const {data} = await api.get<Guitars>(`${APIRoute.Guitars}?price_gte=10000&price_lte=30000&_start=0&_end=5`);
+
+      // GET /guitars?_start=10&_end=20
+      // GET /guitars?_start=20&_end=30
+      // GET /guitars?_start=20&_limit=10
+
+      // Операторы
+      // _gte или _lte для получения в диапазоне
+      // GET /guitars?price_gte=20000&price_lte=30000
+
+      dispatch(loadGuitarsData(data));
+    } catch {
+      // TODO: в случае недоступности сервера отображается информационное сообщение
+      // eslint-disable-next-line no-console
+      console.log('не удалось загрузить отфильтрованные данные');
     }
   };
 
 export {
   fetchGuitarsAction,
-  fetchSortOrderAction
+  fetchSortOrderAction,
+  fetchFilterAction
 };
