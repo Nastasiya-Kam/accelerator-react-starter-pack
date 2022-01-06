5060a8fb1f61cb2928f91db8c3e8497d677ff7b2
Анастасия <kamanina963@gmail.com>
2022-01-06 19:58:16 +0300

    test: Добавь тест для роутинга по приложению
    На данном этапе тест провален, т.к. отрисовка страницы 404 требует
    отрисовки компонентов, использующих хуки useSelector. Без компонентов
    header и footer тест работает и проходит проверку

--- src/components/app/app.test.tsx
+++ src/components/app/app.test.tsx
@@ -1,1 +1,32 @@
-export {};
+import { configureMockStore } from '@jedmao/redux-mock-store';
+import { render, screen } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
+import { Provider } from 'react-redux';
+import { Router } from 'react-router-dom';
+import App from './app';
+
+const mockStore = configureMockStore();
+
+const store = mockStore({
+  GUITARS: {isDataLoaded: true},
+  USER: {},
+});
+
+const history = createMemoryHistory();
+const fakeApp = (
+  <Provider store={store}>
+    <Router history={history}>
+      <App />
+    </Router>
+  </Provider>
+);
+
+describe('Application Routing', () => {
+  it('should render "NotFoundScreen" when user navigate to non-existent route', () => {
+    history.push('/non-existent-route');
+    render(fakeApp);
+
+    expect(screen.getByText('404. Страница не найдена')).toBeInTheDocument();
+    expect(screen.getByText(/Вернуться в/i)).toBeInTheDocument();
+  });
+});
