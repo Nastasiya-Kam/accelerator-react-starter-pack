2ff9d7f2f9e56628026b24ccf51fc01f19847d6e
Анастасия <kamanina963@gmail.com>
2022-01-08 15:32:50 +0300

    test: Добавь тест для компонента Header
    Дополнительно установлены недостающие зависимости для тестирования
    действий пользователя

---
+++ src/components/header/header.test.tsx
@@ -0,0 +1,44 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import Header from './header';
+import { Menu } from '../../const';
+import { configureMockStore } from '@jedmao/redux-mock-store';
+import { Provider } from 'react-redux';
+import { makeFakeGuitars } from '../../utils/mocks';
+
+const mockStore = configureMockStore();
+const mockGuitars = makeFakeGuitars();
+
+describe('Component: Header', () => {
+  it('should render correctly', () => {
+    const store = mockStore({
+      GUITARS: {
+        guitars: mockGuitars,
+        isDataLoaded: true,
+      },
+      USER: {},
+    });
+    const history = createMemoryHistory();
+
+    render(
+      <Provider store={store}>
+        <Router history={history}>
+          <Header />
+        </Router>
+      </Provider>,
+    );
+
+    expect(screen.getByAltText(/Логотип/i)).toBeInTheDocument();
+    expect(screen.getByRole('img')).toBeInTheDocument();
+    Menu.Main.map((item) => expect(screen.getByRole('link', { name: item })).toBeInTheDocument());
+    expect(screen.getByRole('button', { name: 'Начать поиск' })).toBeInTheDocument();
+    expect(screen.getByRole('link', { name: 'Корзина' })).toBeInTheDocument();
+    expect(screen.getByLabelText(/Поиск/i)).toBeInTheDocument();
+
+    userEvent.type(screen.getByLabelText(/Поиск/i), 'chester');
+
+    expect(screen.getByDisplayValue(/chester/i)).toBeInTheDocument();
+  });
+});
