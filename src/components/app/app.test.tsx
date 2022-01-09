fd90ad7606bdd6932e47bc7a4944615ad1fd2b13
Анастасия <kamanina963@gmail.com>
2022-01-09 13:23:15 +0300

    test: Дополни тесты для роутинга по приложению

--- src/components/app/app.test.tsx
+++ src/components/app/app.test.tsx
@@ -2,22 +2,37 @@ import { configureMockStore } from '@jedmao/redux-mock-store';
 import { render, screen } from '@testing-library/react';
 import { createMemoryHistory } from 'history';
 import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 import { Router } from 'react-router-dom';
+import { AppRoute } from '../../const';
 import { makeFakeGuitars } from '../../utils/mocks';
 import App from './app';
 
 const mockGuitars = makeFakeGuitars();
-const mockStore = configureMockStore();
+const mockStore = configureMockStore([thunk]);
 
+const history = createMemoryHistory();
 const store = mockStore({
   GUITARS: {
     guitars: mockGuitars,
+    firstMinPrice: 1000,
+    firstMaxPrice: 50000,
+    pageCount: 3,
     isDataLoaded: true,
   },
-  USER: {},
+  USER: {
+    minPrice: '5000',
+    maxPrice: '15000',
+    types: [],
+    strings: [],
+    sorting: '',
+    order: '',
+    currentPage: 1,
+    currentPageCount: 0,
+    firstPage: 1,
+    lastPage: 3,
+  },
 });
-
-const history = createMemoryHistory();
 const fakeApp = (
   <Provider store={store}>
     <Router history={history}>
@@ -27,6 +42,13 @@ const fakeApp = (
 );
 
 describe('Application Routing', () => {
+  it('should render "CatalogScreen" when user navigate to "/"', () => {
+    history.push(AppRoute.Root);
+    render(fakeApp);
+
+    expect(screen.getByText(/каталог гитар/i)).toBeInTheDocument();
+  });
+
   it('should render "NotFoundScreen" when user navigate to non-existent route', () => {
     history.push('/non-existent-route');
     render(fakeApp);
