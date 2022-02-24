293b1ea592459140082c7ac732ba2f03954bce94
Анастасия <kamanina963@gmail.com>
2022-02-24 10:57:44 +0300

    test: Добавь тесты

--- src/store/cart-data/cart-data.test.ts
+++ src/store/cart-data/cart-data.test.ts
@@ -1,5 +1,5 @@
 import { makeFakeGuitarCart, makeFakeGuitarsCart } from '../../utils/mocks';
-import { addToCart, loadCartData } from '../action';
+import { addToCart, loadCartData, loadDiscount } from '../action';
 import { cartData } from './cart-data';
 
 const mockGuitars = makeFakeGuitarsCart(3);
@@ -10,6 +10,7 @@ describe('Reducer: cart-data', () => {
   it('should loaded guitar in the cart', () => {
     const state = {
       guitarsInCart: [],
+      discount: 0,
     };
 
     expect(cartData(state, loadCartData(mockGuitars)))
@@ -19,9 +20,22 @@ describe('Reducer: cart-data', () => {
   it('should add guitar to the cart', () => {
     const state = {
       guitarsInCart: mockGuitars,
+      discount: 0,
     };
 
     expect(cartData(state, addToCart(mockGuitar)))
       .toEqual({...state, guitarsInCart: [ ...state.guitarsInCart, mockGuitar ]});
+  });
+
+  it('should load discount', () => {
+    const state = {
+      guitarsInCart: [],
+      discount: 0,
+    };
+
+    const discount = 35;
+
+    expect(cartData(state, loadDiscount(discount)))
+      .toEqual({...state, discount: discount});
   });
 });
